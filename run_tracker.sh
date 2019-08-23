22896f54416ac06f80d4c4415f12ca0480358ec6
Cal Kashevarof <33500695+Bering-Sea@users.noreply.github.com>
2019-08-23 11:59:39 -0800

    Update run_tracker.sh

--- run_tracker.sh
+++ run_tracker.sh
@@ -2,7 +2,8 @@
 #
 # This is just a simple script that loads the correct python enviroment
 # and then loops forever restarting the tracker program if it ever exits.
-#
+# if youre running it on adsb-receiver do not change directory. 
+# if youre not change directory to cd /home/pi/AboveTustin below
 
 cd /home/pi/adsb-receiver/build/abovetustin/AboveTustin
 source py3/bin/activate
